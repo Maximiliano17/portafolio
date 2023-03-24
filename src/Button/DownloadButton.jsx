1f79b6ec21462d66b1e04828b8beb750ecbb48cf
Maximiliano17 <maxidossantosburgos@gmail.com>
2023-03-23 23:54:57 -0300

    Adicion de estilos al header y home

---
+++ src/Button/DownloadButton.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "./DownloadButton.scss";
+
+const DownloadButton = ({ url, label }) => {
+  const [isDownloading, setIsDownloading] = useState(false);
+
+  const handleClick = () => {
+    setIsDownloading(true);
+    setTimeout(() => setIsDownloading(false), 3000); // Simulamos una descarga que dura 3 segundos
+  };
+
+  return (
+    <button className={`download-button ${isDownloading ? "downloading" : ""}`} onClick={handleClick}>
+      {isDownloading ? "/" : label}
+    </button>
+  );
+};
+
+export default DownloadButton;
